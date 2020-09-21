6d987c72fc8bcc3b3dfa4cc151a43b25e8089c9c
kaline <mesquitabfkaline@gmail.com>
2020-09-21 04:56:50 -0300

    :lipstick: Format components

--- src/Components/MyRepositoryComponent.js
+++ src/Components/MyRepositoryComponent.js
@@ -1,20 +1,19 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
 
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
   },
   bullet: {
-    display: 'inline-block',
-    margin: '0 2px',
-    transform: 'scale(0.8)',
+    display: "inline-block",
+    margin: "0 2px",
+    transform: "scale(0.8)",
   },
   title: {
     fontSize: 14,
@@ -31,22 +30,26 @@ export default function MyRepositoryComponent(props) {
     <Card id="cardRepository" className={classes.root}>
       <CardContent>
         <Typography variant="h5" component="h2">
-        {props.name}
-       
+          {props.name}
         </Typography>
-       
+
         <Typography variant="body2" component="p">
-        {props.repositorydescr}
+          {props.repositorydescr}
           <br />
         </Typography>
-        <Typography className={classes.title} color="textSecondary" gutterBottom>
-         {props.language}
+        <Typography
+          className={classes.title}
+          color="textSecondary"
+          gutterBottom
+        >
+          {props.language}
         </Typography>
       </CardContent>
       <CardActions>
-      <Button size="small"><a href={props.repositoryURL}>See</a></Button>
+        <Button size="small">
+          <a href={props.repositoryURL}>See</a>
+        </Button>
       </CardActions>
     </Card>
   );
-
 }
--- src/Components/MyUserComponent.js
+++ src/Components/MyUserComponent.js
@@ -1,61 +1,52 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import ApolloClient from 'apollo-boost';
-import { ApolloProvider } from '@apollo/react-hooks';
-import { gql } from 'apollo-boost';
-import { useQuery } from '@apollo/react-hooks';
-import Avatar from '@material-ui/core/Avatar';
-import App from '../App';
-import { validate } from 'graphql';
-import { Card } from '@material-ui/core';
-import MyRepositoryComponent from './MyRepositoryComponent'
-
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { gql } from "apollo-boost";
+import { useQuery } from "@apollo/react-hooks";
+import Avatar from "@material-ui/core/Avatar";
+import MyRepositoryComponent from "./MyRepositoryComponent";
 
 const client = new ApolloClient({
-  uri: 'https://api.github.com/graphql',
- 
+  uri: "https://api.github.com/graphql",
 });
 
-
 const USER = gql`
-query($number_of_repos:Int!, $user_name:String!) {
-  user(login: $user_name) {
-    url
-    avatarUrl
-    name
-    login
-    bio
-    company
-    twitterUsername
-    location
-    email
-    status {
-      id
-    }
-     repositories(last: $number_of_repos) {
-       nodes {
-         name
-         description
-         url
-         primaryLanguage {
+  query($number_of_repos: Int!, $user_name: String!) {
+    user(login: $user_name) {
+      url
+      avatarUrl
+      name
+      login
+      bio
+      company
+      twitterUsername
+      location
+      email
+      status {
+        id
+      }
+      repositories(last: $number_of_repos) {
+        nodes {
           name
-         }
-       }
+          description
+          url
+          primaryLanguage {
+            name
+          }
+        }
+      }
     }
-    
-    }
-}
+  }
 `;
 
 const MyUserComponent = () => {
-if(window.location.pathname == "/"){
-  window.location.pathname = "/kaline";
-}
+  if (window.location.pathname == "/") {
+    window.location.pathname = "/kaline";
+  }
 
   const { loading, error, data } = useQuery(USER, {
     variables: {
-      "number_of_repos": 100,
-      "user_name": window.location.pathname.substring(1)
+      number_of_repos: 100,
+      user_name: window.location.pathname.substring(1),
     },
   });
 
@@ -68,33 +59,39 @@ if(window.location.pathname == "/"){
 
   if (data) {
     const { user } = data;
-    const elements =  user.repositories.nodes;
+    const elements = user.repositories.nodes;
 
     const items = [];
 
     for (const [index, value] of elements.entries()) {
-      items.push(<MyRepositoryComponent  repositoryURL= {value.url} name={value.name} repositorydescr={value.description}  language ={value.primaryLanguage?.name ?? "No language"}/>);
+      items.push(
+        <MyRepositoryComponent
+          repositoryURL={value.url}
+          name={value.name}
+          repositorydescr={value.description}
+          language={value.primaryLanguage?.name ?? "No language"}
+        />
+      );
     }
-  
+
     return (
       <div>
         <section id="profileData">
-        <Avatar id="profileImage" src= { user.avatarUrl } >K</Avatar>
-         <h1><a href={user.url}>{ user.login}</a></h1>
-         <p>{ user.name }</p>
-         <p>{ user.bio }</p>
-         <p>{ user.company }</p>
-         <p>{ user.location }</p>
-         <p>@{ user.twitterUsername }</p>
-         <p>{ user.email }</p>
+          <Avatar id="profileImage" src={user.avatarUrl}>
+            K
+          </Avatar>
+          <h1>
+            <a href={user.url}>{user.login}</a>
+          </h1>
+          <p>{user.name}</p>
+          <p>{user.bio}</p>
+          <p>{user.company}</p>
+          <p>{user.location}</p>
+          <p>@{user.twitterUsername}</p>
+          <p>{user.email}</p>
         </section>
-        
 
-         <div className='rowC'>
-         { items }
-        </div>
-       
-          
+        <div className="rowC">{items}</div>
       </div>
     );
   }
